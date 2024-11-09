554108b3ee71f0fa0b7c4a2f0f7e4d27ee41a33a
rubasalmuziraee <109699290+rubasalmuziraee@users.noreply.github.com>
2024-11-09 22:53:40 +0300

    Add files via upload

---
+++ faceDetection-master/main.py
@@ -0,0 +1,19 @@
+#importing the OpenCV Lib
+import cv2
+#Loading the haarcascade front face file
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#Reading the face image
+#img = cv2.imread('test1.jpeg')
+#img = cv2.imread('test2.png')
+img = cv2.imread('test3.jpg')
+#Converting the image into grayscale image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Detecting the face
+face = faceCascade.detectMultiScale(gray, 1.1,4)
+#Draw rectangle around the face
+for(x,y,w,h) in face:
+    cv2.rectangle(img, (x,y),(x+w, y+h), (255,0,0),2)
+#Display the result with face detected
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
